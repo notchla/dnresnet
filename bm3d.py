3b4187c3ee93d659ca3f809512d2d6bf52978b16
notchla <liso.lorenzo@gmail.com>
2021-06-28 16:16:21 +0200

    added utils

---
+++ bm3d.py
@@ -0,0 +1,43 @@
+import numpy as np
+from bm3d import bm3d
+from experiment_funcs import get_experiment_noise, get_psnr, get_cropped_psnr
+
+ # Experiment specifications
+    imagename = 'cameraman256.png'
+
+    # Load noise-free image
+    y = np.array(Image.open(imagename)) / 255
+    # Possible noise types to be generated 'gw', 'g1', 'g2', 'g3', 'g4', 'g1w',
+    # 'g2w', 'g3w', 'g4w'.
+    noise_type = 'g3'
+    noise_var = 0.02  # Noise variance
+    seed = 0  # seed for pseudorandom noise realization
+
+    # Generate noise with given PSD
+    noise, psd, kernel = get_experiment_noise(noise_type, noise_var, seed, y.shape)
+    # N.B.: For the sake of simulating a more realistic acquisition scenario,
+    # the generated noise is *not* circulant. Therefore there is a slight
+    # discrepancy between PSD and the actual PSD computed from infinitely many
+    # realizations of this noise with different seeds.
+
+    # Generate noisy image corrupted by additive spatially correlated noise
+    # with noise power spectrum PSD
+    z = np.atleast_3d(y) + np.atleast_3d(noise)
+
+    # Call BM3D With the default settings.
+    y_est = bm3d(z, psd)
+
+    # To include refiltering:
+    # y_est = bm3d(z, psd, 'refilter')
+
+    # For other settings, use BM3DProfile.
+    # profile = BM3DProfile(); # equivalent to profile = BM3DProfile('np');
+    # profile.gamma = 6;  # redefine value of gamma parameter
+    # y_est = bm3d(z, psd, profile);
+
+    # Note: For white noise, you may instead of the PSD
+    # also pass a standard deviation
+    # y_est = bm3d(z, sqrt(noise_var));
+
+    psnr = get_psnr(y, y_est)
+    print("PSNR:", psnr)
